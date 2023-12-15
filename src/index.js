13e7fc541c16da58dc86872ba7add348368fda11
saraporri <saraporricino@yahoo.com>
2023-12-15 15:42:52 +0100

    Fixed api and changed strong style

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function generateLyrics(event) {
   let apiKey = "8b07dt8e17fee9f438eo54a03e745179";
   let prompt = `User instructions: Generate song lyrics about ${instructionsInput.value}`;
   let context =
-    "Please it should be just a verse, everything in basic HTML and separate each line with a <br/>. Include song title and artist at the end inside a <strong>. Make sure to follow the user instructions";
+    "Please it should be just a verse, everything in basic HTML and separate each line with a <br/>. Include song title and artist inside a <strong> element. Make sure to follow the user instructions";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   axios.get(apiURL).then(displayLyrics);
 }
